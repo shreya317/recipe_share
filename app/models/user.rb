a3e70801ff4f4ab15b5f84aa8d93d656381a4033
Shreya Patel <shreyapatel@Shreya-Patels-MacBook-Pro.local>
2015-03-03 16:22:47 -0600

    enabled bcrypt and has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-	  # validates_presence_of :first_name, :last_name, :email, :password_digest
+    has_secure_password
+
+    has_many :recipes
+
+	  validates_presence_of :first_name, :last_name, :email, :password_digest
 end
