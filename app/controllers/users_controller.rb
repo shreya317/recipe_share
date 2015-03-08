f40c7eb91348ebd99e5b6f623cbdb91122cbef75
Shreya Patel <shreya317@gmail.com>
2015-03-08 14:52:44 -0500

    changed path for new account and precompiled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
     if @user.save
-      redirect_to login_path
+      redirect_to welcome_index_path
     else
       render :new
     end
